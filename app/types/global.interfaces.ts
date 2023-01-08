8c0b9ce84168efca184f04698175bcab6e2c75f1
Noevain <noevain@gmail.com>
2023-01-08 19:01:46 +0100

    ajout d autres objets au shop

--- app/types/global.interfaces.ts
+++ app/types/global.interfaces.ts
@@ -11,5 +11,9 @@
 
 const things = [
     {id:0, name: 'Club de golf',pngpath:'golf.jpg',price:49},
+    {id:1, name: 'Tapis de souris',pngpath:'souris.jpg',price:29},
+    {id:2,name: 'Chassures de course',pngpath:'course.jpg',price:59},
+    {id:3,name: 'Trampoline',pngpath:'trampo.jpg',price:999},
+    {id:4,name: 'Raquette de Tenis',pngpath:'tennis.jpg',price:99},
 ]
 export { things }
